b81ff7d3d275d7cf9c577d90e2f259c4384baa65
minkyung <minkyungww@naver.com>
2022-10-22 17:37:15 +0900

    코테

--- co_test/co_test120810.py
+++ co_test/co_test120810.py
@@ -1,5 +1,23 @@
-def solution(num1 : int, num2 : int):
-    answer = num2 % num1
-    return answer
 
-print(solution(3,2))
+
+# def solution(num1 : int, num2 : int):
+#     answer = num2 % num1
+#     return answer
+
+# print(solution(3,2))
+
+
+
+# array = [180, 120,140]
+# height = [190]
+# i = array + height
+# i.sort(reverse=True)
+# print(i)
+
+def solution(array, height):
+    array = list[input()]
+    height = list[input()]
+    i = array + height
+    i.sort(reverse = True)
+    return(i.index(190))
+# 머쓱이보다 키 큰 사람
